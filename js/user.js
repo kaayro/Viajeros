125745b58221aa7d461caada14d590e9051ee6c2
Carlos Celma <solrac@igitsoft.com>
2016-11-29 16:47:10 -0600

    k

--- js/user.js
+++ js/user.js
@@ -8,11 +8,8 @@ var user = {
 		var date = $("#rbdate").val();
 		
 		if(name =! undefined && name != '' && mail != undefined && mail != '' && pass != undefined && pass != '' && coun != undefined && coun != '' && lang != undefined && lang != '' && date != undefined && date != ''){
-			$.post('http://tourindigital.com/app/registro.app.php', {action:'setNewUser',name:name,mail:mail,pass:pass,country:coun,languages:lang,bdate:date}, function(msg){
-			alert(msg);
-		}, function(e){
-			alert("error");
-		});
+			var data = { action:'setNewUser' };//,name:name,mail:mail,pass:pass,country:coun,languages:lang,bdate:date};
+			app.ajax('http://tourindigital.com/app/registro.app.php',data);
 		}else
 			alert("Todos los campos son requeridos");
 	}
